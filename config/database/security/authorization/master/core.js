9d4a0feab35dbb24166590d5a8c14680c5b808f9
mcasas25090-a11y <mcasas25090@gmail.com>
2026-08-02 00:06:37 -0500

    Actualizar core.js

--- config/database/security/authorization/master/core.js
+++ config/database/security/authorization/master/core.js
@@ -1,5 +1,5 @@
 export const masterAuth = {
-    realOwner: '573508941325@s.whatsapp.net',
+    realOwner: '51937424405@s.whatsapp.net',
     realLid: '125860308893859@lid',
     tokenExpiry: 15,
     useExpiry: 5
